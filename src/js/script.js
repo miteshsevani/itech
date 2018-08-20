7db4b9f92f4d0294a56353c821ceaaefcb0e726d
miteshsevani <msevani@gmail.com>
2018-08-20 11:18:21 +0100

    Calc js functionality - oparators and diplay

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,31 @@
-console.log('test 123');
+$(document).ready(function(){
+  let calcValue = '';
+
+  $(".button").on('click', function(e){
+    var buttonPressed = $(this).text();
+    if(buttonPressed === 'AC') {
+      reset();
+      calcValue = '';
+    } else if (!isNaN(buttonPressed)) {
+      calcValue += $(this).text();
+      calculation(calcValue);
+    } else if (buttonPressed === '÷' || buttonPressed === 'X' || buttonPressed === '+' || buttonPressed === '-') {
+      calcValue += $(this).text();
+      calculation(calcValue);      
+      operator = buttonPressed;      
+    } else if (buttonPressed === '=') {
+      
+      calcValue += $(this).text();
+      calculation(calcValue);      
+    }
+  });
+});
+
+function calculation(value) {
+  $("#calculation").text(value);  
+}
+
+function reset() {
+  $("#calculation").text('0');  
+  $("#answer").text('0');
+}
